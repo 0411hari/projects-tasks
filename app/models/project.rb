984048ef3e4deb6538c98a1359adaf0238efc819
0411hari <krishnab5118@gmail.com>
2022-01-10 19:39:38 +0530

    jan 10

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,5 +3,5 @@ class Project < ApplicationRecord
   belongs_to :user
   validates :name, presence: true, :uniqueness => true,
   validates :start_date, presence: true
-  validates :end_date, presence: true,
+  validates :end_date, presence: true
 end
